926843559bfc10aed80c5d6daa8f37fc4ba4bfee
nmf_wasi <wasi7741@gmail.com>
2026-08-25 13:17:19 +0900

    added kpop models

--- app/config/enums.py
+++ app/config/enums.py
@@ -5,3 +5,8 @@ class UserRole(str, Enum):
     """Add user roles here, when new roles are added, do alembic migrations"""
     USER = "user"
     ADMIN = "admin"
+
+class GenderChoice(str, Enum):
+    """Gives Gender choice to idols"""
+    MALE="male"
+    FEMALE="female"
--- app/models/models.py
+++ app/models/models.py
@@ -1,8 +1,9 @@
 from app.database.database import Base
-from sqlalchemy.orm import mapped_column, Mapped
-from sqlalchemy import Integer, String, DateTime
-from app.config.enums import UserRole
+from sqlalchemy.orm import mapped_column, Mapped, relationship
+from sqlalchemy import Integer, String, DateTime, Date, ForeignKey, Index, Enum as SQLEnum
+from app.config.enums import UserRole, GenderChoice
 from datetime import datetime
+from datetime import date
 
 
 class User(Base):
@@ -26,4 +27,109 @@ class User(Base):
     )
     last_login: Mapped[datetime | None] = mapped_column(
         DateTime, nullable=True, default=None
+    )
+
+
+class Song(Base):
+    """Song table, has Mto1 relationship with ALbum"""
+
+    __tablename__ = "songs"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String)
+    release_date: Mapped[date] = mapped_column(Date)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now)
+    last_update: Mapped[datetime] = mapped_column(DateTime, default=datetime.now)
+    album_id: Mapped[int | None] = mapped_column(  # now nullable
+        Integer, ForeignKey("albums.id", ondelete="SET NULL"), nullable=True
+    )  # the FK always lives on the "many" side
+
+    album: Mapped["Album|None"] = relationship(back_populates="songs")
+    # back populates song col on Album table
+
+    __table_args__ = (
+        Index("idx_song_title", "title"),
+        Index("idx_song_album_id", "album_id"),
+    )
+
+
+class Album(Base):
+    """Album table, has 1toM relation with Songs and Mto1 with Group"""
+
+    __tablename__ = "albums"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    release_date: Mapped[date] = mapped_column(Date)
+    author: Mapped[str | None] = mapped_column(String, nullable=True)
+    songs: Mapped[list["Song"]] = relationship(
+        back_populates="album",  # write the table_name, not the className
+        passive_deletes=True,  # let the db handle the SET NULL,
+    )
+    group_id: Mapped[int | None] = mapped_column(
+        Integer,
+        ForeignKey(
+            "groups.id",
+            ondelete="SET NULL",
+        ),
+        nullable=True,
+    )
+    group: Mapped["Group"] = relationship(
+        back_populates="albums",
+    )
+    slug: Mapped[str] = mapped_column(
+        String,
+        unique=True,
+        index=True,
+    )
+
+    __table_args__ = (Index("idx_album_group_id", "group_id"),)
+
+
+class Group(Base):
+    """Group Table, has 1toM relation with both Albums and Idols"""
+
+    __tablename__ = "groups"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String, unique=True)
+    albums: Mapped[list["Album"]] = relationship(
+        back_populates="group",
+        passive_deletes=True,  # Trust db to set null, no need for manual handling
+    )
+    members: Mapped[list["Idol"]] = relationship(
+        back_populates="group",
+        passive_deletes=True,
+    )
+    slug: Mapped[str] = mapped_column(String, unique=True, index=True)
+
+
+class Idol(Base):
+    """has 1toM wth Groups, so hold the fk, back_populates group's members col"""
+
+    __tablename__ = "idols"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    stage_name: Mapped[str] = mapped_column()
+    full_name: Mapped[str | None] = mapped_column(nullable=True)
+    korean_name: Mapped[str | None] = mapped_column(nullable=True)
+    korean_stage_name: Mapped[str | None] = mapped_column(nullable=True)
+    birth_date: Mapped[date | None] = mapped_column(nullable=True)
+    birth_place: Mapped[str | None] = mapped_column(nullable=True)
+    group_id: Mapped[int | None] = mapped_column(
+        Integer,
+        ForeignKey(
+            "groups.id",
+            ondelete="SET NULL",
+        ),
+        nullable=True,
+    )
+    group: Mapped["Group|None"] = relationship(back_populates="members")
+    country: Mapped[str | None] = mapped_column(nullable=True)
+    instagram_username: Mapped[str | None] = mapped_column(nullable=True)
+    gender: Mapped[GenderChoice|None] = mapped_column(SQLEnum(GenderChoice),nullable=True)
+    slug = mapped_column(String, unique=True, index=True)
+
+    __table_args__ = (
+        Index("idx_idol_country", "country"),
+        Index("idx_idol_group_id", "group_id"),
+        Index("idx_idol_stage_name", "stage_name"),
     )
