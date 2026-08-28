cd0f2352d3abc556fb6f5f000db3a729247564d0
nmf_wasi <wasi7741@gmail.com>
2026-08-28 11:23:03 +0900

    rate limiter applied

--- app/src/main.py
+++ app/src/main.py
@@ -2,14 +2,17 @@ from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from app.routers import users, idols, songs, groups, albums
 from app.database.database import get_db
+from app.middleware.rate_limiter import rate_limiter
 
 app = FastAPI()
-
+app.middleware("http")(rate_limiter)
+# registers a middleware for the HTTP protocol, Calling it with (rate_limiter) immediately after is Python's decorator mechanics applied manually
 app.include_router(users.router, prefix="/api/users", tags=["users"])
 app.include_router(idols.router, prefix="/api/idols", tags=["idols"])
-app.include_router(songs.router, prefix='/api/songs', tags=['songs'])
-app.include_router(groups.router, prefix='/api/groups', tags=['groups'])
-app.include_router(albums.router, prefix='/api/albums', tags=['albums'])
+app.include_router(songs.router, prefix="/api/songs", tags=["songs"])
+app.include_router(groups.router, prefix="/api/groups", tags=["groups"])
+app.include_router(albums.router, prefix="/api/albums", tags=["albums"])
+
 
 @app.get("/")
 def home(dh: Session = Depends(get_db)):
@@ -17,4 +20,4 @@ def home(dh: Session = Depends(get_db)):
 
 
 for f in ["users", "idols", "songs", "groups", "albums"]:
-    print(f"app.include_router({f}.router, prefix='/api/{f}', tags=['{f}'])") 
+    print(f"app.include_router({f}.router, prefix='/api/{f}', tags=['{f}'])")
